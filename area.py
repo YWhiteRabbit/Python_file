c6b1caa600b8540953abf22d1817a15cc27639b5
Yana <48756337+YWhiteRabbit@users.noreply.github.com>
2023-11-08 11:46:37 +0200

    Update area.py

--- area.py
+++ area.py
@@ -1,24 +1,21 @@
 import math
 
-def figure_inf(figure, x1, y1, x2, y2):
+def figure_rectengle(x1, y1, x2, y2):
     '''(x1, y1)-нижня ліва точка, (x2, y2)-верхня права точка
 
     визначимо (x3, y3)-верхню ліву, (x4, y4)-нижню праву'''
-    if figure=='squre':
-        side_figure_squre=side(x1, y1, x2, y2)
-        area_figure_squre=area_squre(side_figure_squre)
-        perim_squre=perimetre_squre(side_figure_squre)
-        print('Squre Area:{} Perimeter:{}'.format(area_figure_squre, perim_squre))
-    elif figure=='rectangle':
-        x3=x1
-        y3=y2
-        x4=x2
-        y4=y1
-        side_1=side(x1, y1, x3, y3)
-        side_2=side(x1, y1, x4, y4)
-        perim_regt=perimitre_regtengle(side_1, side_2)
-        area_regt=area_regtengle(side_1, side_2)
-        print('Squre Area:{} Perimeter:{}'.format(perim_regt, area_regt))
+    x3 = x1
+    y3 = y2
+    x4 = x2
+    y4 = y1
+    side_1 = side(x1, y1, x3, y3)
+    side_2 = side(x1, y1, x4, y4)
+    perim_rect = perimitre_rectengle(side_1, side_2)
+    area_regt = area_regtengle(side_1, side_2)
+    return 'Rectangle Area:{} Perimeter:{}'.format(perim_rect, area_regt)
+
+def figure_square(side_square):
+    return 'Square Area:{} Perimeter:{}'.format(perimetre_squre(side_square), area_square(side_square))
 
 def side(x_1, y_1, x_2, y_2):
     '''знаходимо сторону фігури з двома точками'''
@@ -27,10 +24,10 @@ def side(x_1, y_1, x_2, y_2):
 def perimetre_squre(side):
     return 4*side
 
-def perimitre_regtengle(side1, side2):
+def perimitre_rectengle(side1, side2):
     return (side1*2)+(side2*2)
 
-def area_squre(side_squre):
+def area_square(side_squre):
     '''знаходимо площу квадрату'''
     return side_squre**2
 
@@ -38,45 +35,48 @@ def area_regtengle(side1, side2):
     '''знаходимо площу прямокутника'''
     return side1*side2
 
-def triangle_info(x1, x2, x3, y1, y2, y3):
-    side1=side(x1, y1, x2, y2)
-    side2=side(x2, y2, x3, y3)
-    side3=side(x3, y3, x1, y1)
-    per_tr=perimetr_triangle(side1, side2, side3)
-    area_tr=area_triangle(side1, side2, side3)
+def figure_triangle(x1, x2, x3, y1, y2, y3):
+    side1 = side(x1, y1, x2, y2)
+    side2 = side(x2, y2, x3, y3)
+    side3 = side(x3, y3, x1, y1)
+    per_tr = perimetr_triangle(side1, side2, side3)
+    area_tr = area_triangle(side1, side2, side3)
     print('Triangle Area:{} Perimeter:{}'.format(per_tr, area_tr))
 
 def area_triangle(side_1, side_2, side_3):
-    p=(side_1+side_2+side_3)/2
+    p = (side_1+side_2+side_3)/2
     return (p*(p-side_1)*(p-side_2)*(p-side_3))**(1/2)
 
 def perimetr_triangle(side_1, side_2, side_3):
     return side_1+side_2+side_3
 
-def sircle_info(r):
-    area_sircle=math.pi*(r**2)
-    perim=2*math.pi*r
-    print('Sircle Area:{} Perimeter:{}'.format(perim, area_sircle))
+def figure_circle(r):
+    area_sircle = math.pi*(r**2)
+    perim = 2*math.pi*r
+    print('Circle Area:{} Perimeter:{}'.format(perim, area_sircle))
 
-figur=['squre', 'circle', 'rectangle', 'triangle']
-fig=input('Enter figure:')
-if fig not in figur:
-    print('NameError')
-if fig in figur:
-    if fig=='rectangle' or fig=='squre':
-        k_1=int(input('x1:'))
-        k_2=int(input('x2:'))
-        r_1=int(input('y1:'))
-        r_2=int(input('y2:'))
-        print(figure_inf(fig, k_1, r_1, k_2, r_2))
-    if fig=='tringle':
-        k_1 =int(input('x1:'))
-        r_1 =int(input('y1:'))
-        k_2 =int(input('x2:'))
-        r_2 =int(input('y2:'))
-        k_3 =int(input('x3:'))
-        r_3 =int(input('y3:'))
-        print(triangle_info(k_1, k_2, k_3, r_1, r_2, r_3))
-    if fig=='circle':
-        radius=int(input('Radius:'))
-        print(sircle_info(radius))
+#вводиться фігура, розташування, точки
+
+print('This program calculates the area and perimeter for a square, rectangle, triangle, and circle. ')
+print('If you want to exit the app, enter the number 5.')
+
+while True:
+    f = input('Enter your figure geometriqe:')
+    if f=='square':
+        location, x_1, y_1, side_1 = map(input('Maintain shape data separated by a space:'))
+        print(figure_square(side_1))
+    elif f=='rectangle':
+        location1, x_1, y_1, location2, x_2, y_2 = map(input('Maintain shape data separated by a space:'))
+        print(figure_rectengle(x_1, y_1, x_2, y_2))
+    elif f=='circle':
+        location, x_1, y_1, radius = map(input('Maintain shape data separated by a space:'))
+        print(figure_circle(radius))
+    elif f=='triangle':
+        p1, x_1, y_1, p_2, x_2, y_2, p3, x_3, y_3 = map((input('Maintain shape data separated by a space:')))
+        print(figure_triangle(x_1, x_2, x_3, y_1, y_2, y_3))
+    elif f=='5':
+        print('By!')
+        break
+    else:
+        print('NameError: There is no figure with this name. Please enter one of the following names:'
+              'square, rectangle, circle, triangle')
